e5fedf891cfd0f98673720441da5d03acb2bd6cc
Jonathan Gai <jonathangai2003@gmail.com>
2020-04-20 19:22:43 -0400

    Add Merg Sort visualization

--- vsort.py
+++ vsort.py
@@ -1,15 +1,17 @@
 from matplotlib import pyplot as plt
 from matplotlib import animation
+import math
 
 
 class Sort:
 
     def __init__(self, length):
         self.value = [i + 1 for i in range(length)]
+        self.data_length = length
         self.data = {}
         self.data["insert"] = self.insert()
         self.data["bubble"] = self.bubble()
-        self.data_length = length
+        self.data["merge"] = self.merge()
         self.sort_length = len(self.data)
         self.time_length = max([len(self.data[item]) for item in self.data])
 
@@ -35,6 +37,55 @@ class Sort:
                     data.append(li[:])
         return data
 
+    def merge(self):
+        # Weird argument for the ease of visualization.
+        def mergeSort(li, pos):
+            if len(li) > 1:
+                left = li[:len(li) // 2]
+                right = li[len(li) // 2:]
+                mergeSort(left, [*pos, 0])
+                mergeSort(right, [*pos, 1])
+                i = 0
+                j = 0
+                while (i < len(left)) and (j < len(right)):
+                    if left[i] > right[j]:
+                        li[i + j] = left[i]
+                        temp[position(pos) + i + j] = left[i]
+                        data.append(temp[:])
+                        i = i + 1
+                    else:
+                        li[i + j] = right[j]
+                        temp[position(pos) + i + j] = right[j]
+                        data.append(temp[:])
+                        j = j + 1
+                while i < len(left):
+                    li[i + j] = left[i]
+                    temp[position(pos) + i + j] = left[i]
+                    data.append(temp[:])
+                    i = i + 1
+                while j < len(right):
+                    li[i + j] = right[j]
+                    temp[position(pos) + i + j] = right[j]
+                    data.append(temp[:])
+                    j = j + 1
+
+        def position(pos):
+            t = self.data_length
+            sum = 0
+            for item in pos:
+                if item == 1:
+                    sum = sum + t // 2
+                    t = math.ceil(t / 2)
+                else:
+                    t = t // 2
+            return sum
+
+        li = self.value[:]
+        temp = self.value[:]
+        data = []
+        mergeSort(li, [])
+        return data
+
     def draw(self):
         def ani(time):
             for idx, item in enumerate(self.data):
@@ -42,7 +93,7 @@ class Sort:
                 ax[idx].bar([i for i in range(self.data_length)],
                             [i for i in self.data[item][time]])
 
-        fig, ax = plt.subplots(2)
+        fig, ax = plt.subplots(self.sort_length)
         anim = animation.FuncAnimation(fig, ani,
                                        frames=range(self.time_length),
                                        interval=1, repeat=False)
